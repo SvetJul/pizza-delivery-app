1f42f68d706e35e7340f6fd6f58ccb696a070994
ysvetlichnaya <ysvetlichnaya@solanteq.com>
2019-12-04 18:07:37 +0300

    hikari connection pool configured

--- src/main/kotlin/club/kuzyayo/pizza/delivery/Db.kt
+++
@@ -1,8 +0,0 @@
-package club.kuzyayo.pizza.delivery
-
-import com.zaxxer.hikari.HikariDataSource
-
-val dataSource = HikariDataSource().apply {
-    username = "postgres"
-    jdbcUrl = "jdbc:postgresql://localhost/bd_course"
-}
